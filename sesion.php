e7a684aa051ccacebb5496a5666ed97f95d45482
HabygailSantos <abygaillopez0@gmail.com>
2022-09-12 22:41:31 -0600

    pagina casi terminada

--- sesion.php
+++ sesion.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="images/iconofavi.png">
+    <link rel="shortcut icon" href="images/iconofavi.png">
+    <link rel="stylesheet" href="/login-form/index.html">
     <title></title>
 </head>
 <body>
@@ -12,7 +14,7 @@
     $usuario = $_POST['Usuario'];
     $contraseña = $_POST['Contraseña'];
 
-    if ($usuario == "admin" && $contraseña == "mensajes") {
+    if ($usuario == "LCP" && $contraseña == "123") {
         echo "Usuario y Contraseña Correcto";
     }else{
         echo "Usuario/ Contraseña incorrecto";
